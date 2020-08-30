afc56fc82d8d7e18ddd3443cef56c3533b9beeab
Pobibi <plokij6802@gmail.com>
2020-08-30 16:41:19 +0900

    remove pang {pangparo} directory

--- pangparo/pang/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from pang.models import Panglist,Pangstore,Panguser
-# Register your models here.
-admin.site.register(Panglist)
-admin.site.register(Pangstore)
-admin.site.register(Panguser)
-
--- pangparo/pang/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PangConfig(AppConfig):
-    name = 'pang'
